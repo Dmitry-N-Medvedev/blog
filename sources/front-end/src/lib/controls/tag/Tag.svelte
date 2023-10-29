90d29bf5838cce0c8833421a1f33068b4a5c7a6d
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2023-10-29 17:32:16 +0100

    white theme

---
+++ sources/front-end/src/lib/controls/tag/Tag.svelte
@@ -0,0 +1,25 @@
+<script>
+  /**
+   * @type {string}
+   */
+  export let value;
+  /**
+   * @type {string}
+   */
+  export let text;
+
+  const handleButtonClick = (e) => {
+    console.log('handleButtonClick', e, value);
+  };
+</script>
+
+<style>
+  button {
+    background-color: transparent;
+    padding: 0.25rem;
+    border: none;
+    cursor: pointer;
+  }
+</style>
+
+<button on:click={handleButtonClick}>{text}</button>
--- sources/front-end/src/routes/+page.svelte
+++ sources/front-end/src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Records from './records.json';
+  import {
+    onMount,
+  } from 'svelte';
+  import Tag from '$lib/controls/tag/Tag.svelte';
+	import { text } from '@sveltejs/kit';
+
+  /**
+   * @type {Array<object>}
+   */
+  let posts = [];
+
+  onMount(() => {
+    console.log(Records);
+
+    posts = Records;
+  });
+</script>
+
+<style>
+  article {
+    display: flex;
+    flex-direction: column;
+    row-gap: 1rem;
+
+    & > .post {
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-template-rows: repeat(3, auto);
+      row-gap: 1rem;
+      padding: 0.5rem;
+      
+      background-color: var(--theme-darker_white);
+
+      & > .tag-list {
+        display: flex;
+        column-gap: 0.5rem;
+      }
+    }
+  }
+</style>
+
+<article>
+  {#each posts as post}
+    <div class="post" href="{post.href}">
+      <h2>{post.title}</h2> 
+      <div class="tag-list">
+        {#each post.tags as tag}
+          <Tag text={tag} value={tag} />
+        {/each}
+      </div>
+      <div class="abstract">{post.abstract}</div>
+      <a class="href" href="{post.href}">{post.href}</a>
+    </div>
+  {/each}
+</article>
